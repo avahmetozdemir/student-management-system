ff5fe4ef07d36c1538fd1f9412ea1630cdb9a950
avahmetozdemir <avamertozdemir@gmail.com>
2022-11-09 22:38:38 +0300

    03- add fields of student class

--- src/com/example/studentmanagementsystem/Student.java
+++ src/com/example/studentmanagementsystem/Student.java
@@ -1,6 +1,14 @@
 package com.example.studentmanagementsystem;
 
 public class Student {
+    private String firstName;
+    private String lastName;
+    private int gradeYear;
+    private String studentID;
+    private String courses = null;
+    private int tuitionBalance = 0;
+    private static int costOfCourse = 600;
+    private static int id = 1000;
 
     //Constructor : prompt user to enter student's name and year
 
--- src/com/example/studentmanagementsystem/StudentManagementApp.java
+++ src/com/example/studentmanagementsystem/StudentManagementApp.java
@@ -1,13 +1,26 @@
 package com.example.studentmanagementsystem;
 
+import java.util.Scanner;
+
 public class StudentManagementApp {
     public static void main(String[] args) {
 
         //How many new students we want to add
-
+        System.out.println("Enter number of new student to enroll: ");
+        Scanner in = new Scanner(System.in);
+        int numberOfStudents = in.nextInt();
+        Student[] students = new Student[numberOfStudents];
 
         //Create n number of new students
 
+        for (int n = 0; n < numberOfStudents; n++){
+            students[n] = new Student();
+
+        }
+        for (int n = 0; n < numberOfStudents; n++){
+
+            System.out.println(students[n].toString());
+        }
 
     }
 }
