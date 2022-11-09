7d2867a6c858207294454b04edbeedeb03894a00
avahmetozdemir <avamertozdemir@gmail.com>
2022-11-09 22:36:36 +0300

    wrote steps for program

--- src/com/example/studentmanagementsystem/Student.java
+++ src/com/example/studentmanagementsystem/Student.java
@@ -1,4 +1,20 @@
 package com.example.studentmanagementsystem;
 
 public class Student {
+
+    //Constructor : prompt user to enter student's name and year
+
+    // Generate an ID
+
+
+    //Enroll in courses
+
+
+    //View balance
+
+    //Pay tuition
+
+
+    //Show status
+
 }
--- src/com/example/studentmanagementsystem/StudentManagementApp.java
+++ src/com/example/studentmanagementsystem/StudentManagementApp.java
@@ -3,5 +3,11 @@ package com.example.studentmanagementsystem;
 public class StudentManagementApp {
     public static void main(String[] args) {
 
+        //How many new students we want to add
+
+
+        //Create n number of new students
+
+
     }
 }
